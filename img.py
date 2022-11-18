e0cb96f641d1abb601d338416f1cf4b681e24974
hanifmz07 <13521157@std.stei.itb.ac.id>
2022-11-18 17:55:04 +0700

    feat: change to np.array, return None if face not detected

--- img.py
+++ img.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2
+import cv2 as cv
 import matplotlib.image as image
 import mediapipe as mp
 import matplotlib.pyplot as plt
@@ -7,11 +7,11 @@ import os
 
 # FUNCTIONS
 def resize(filename):
-    resized = cv2.resize(filename, (256, 256))
+    resized = cv.resize(filename, (256, 256))
     return resized
 
 def grayscale(filename):
-    gray = cv2.cvtColor(filename, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(filename, cv.COLOR_BGR2GRAY)
     return gray
 
 def changebg(filename):
@@ -20,10 +20,10 @@ def changebg(filename):
     change_bg_segment = change_background_mp.SelfieSegmentation()
 
     # read image file
-    sample_img = cv2.imread(filename)
+    sample_img = cv.imread(filename)
 
     # convert the BGR format image to an RGB format
-    RGB_sample_img = cv2.cvtColor(sample_img, cv2.COLOR_BGR2RGB)
+    RGB_sample_img = cv.cvtColor(sample_img, cv.COLOR_BGR2RGB)
 
     result = change_bg_segment.process(RGB_sample_img)
 
@@ -35,77 +35,58 @@ def changebg(filename):
 
     # change background color to white
     output_image = np.where(binary_mask_3, sample_img, 255)  
-    # ss = np.asmatrix(output_image)  
 
     # write result to an image file
     return output_image
 
 def cropface(file):
-    # Read the input image
-    # img = cv2.imread(filename)
-    
+
     # Convert into grayscale
-    gray = cv2.cvtColor(file, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(file, cv.COLOR_BGR2GRAY)
     
     # Load the cascade
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+    face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
     
     # Detect faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     
+    if len(faces) != 1:
+        return None
+
     # Draw rectangle around the faces and crop the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(file, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv.rectangle(file, (x, y), (x+w, y+h), (0, 0, 255), 2)
         faces = file[y:y + h, x:x + w]
-        cv2.imshow("face",faces)
-        cv2.imwrite('face.jpg', faces)
         
     # Display the output
-    cv2.imwrite('detcted.jpg', file)
-    cv2.imshow('img', file)
-    cv2.waitKey()
+    # cv.imwrite('detcted.jpg', file)
+    # cv.imshow('img', file)
+    # cv.waitKey()
     return faces
 
 # Function preprocess image
-def preprocess(dir,address,name):
-    S = []
-    # S = [0 for x in range(10)]
+def preprocess(dir):
+    S = np.empty((0, 256*256), int)
     for root, dirs, files in os.walk(dir):
-        i = 0
         for filename in files:
-            print(os.path.join(root, filename))
+            # print(os.path.join(root, filename))
             addr = os.path.join(root, filename)
             # change background to white
             pic = changebg(addr)
             # detect and crop face
             face = cropface(pic)
+
+            if face is None:
+                continue
+                
             # Resize image to 256 x 256
             resized = resize(face)
             # convert to grayscale
             gray_image = grayscale(resized)
-            cv2.imwrite(name + str(i) + '.jpg', gray_image)
-            img = image.imread(name + str(i) + '.jpg')
+            
+            # Check preprocess image
+            # cv.imwrite(f'preprocess/pre_{i}.jpg', gray_image)
+
             # append to array of image matrixs
-            print(img)
-            S.append(img)
-            # S[i] = img
-            # print(S)
-            i += 1
-    # print(S)
-    print(S[1])
+            S = np.append(S, [gray_image.flatten()], axis=0)
     return S
-
-
-# define address and filename
-address = 'C:\\Users\\naufa\\OneDrive\\Documents\\C FIles\\Tubes Algeo 2'
-dir = 'dataset'
-name = "fly"
-file = 'sample.jpeg'
-
-# call function
-preprocess(dir,address,name)
-
-# convert to matrix
-# img = image.imread('C:/Users/naufa/OneDrive/Documents/C FIles/Tubes Algeo 2/fly0.jpg')
-# print('The Shape of the image is:',img.shape)
-# print(img)
