efe222e4128896b4a83cdecc2b598b50bbd200f8
NerbSerg <13521043@mahasiswa.itb.ac.id>
2022-11-22 19:31:26 +0700

    update

--- src/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-k.PhotoImage(Image.open(ImgTest).resize((widthPic, heightPic)))
-TestI = Label(image=TestImage, borderwidth=0.5, bg='dark blue')
-TestResult = ImageTk.PhotoImage(Image.open(ImgResult).resize((widthPic, heightPic)))
-TestR = Label(image=
